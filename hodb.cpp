4f5f7ea55bddd115063c976b1fc91db310b1417f
Aaron France <aaron.l.france@gmail.com>
2017-12-10 17:06:16 +0100

    Remove old/incorrect comment

--- hodb.cpp
+++ hodb.cpp
@@ -46,7 +46,6 @@ byte checksum(byte command_byte, byte send_length, byte address, byte read_lengt
 }
 
 bool checksum_matches(byte packet[20]) {
-    // this only works for RPM right now.
     byte length = packet[1];
     int command_response_checksum = 0;
     for (int i = ECU_RESPONSE_HEADER_SIZE; i < length; i++) {
